ab5805fad20510707a9ac56a6e4cf745f06aa73b
lucas silverio <lucas.silverio@clouddata.com.vc>
2022-01-30 11:37:28 -0300

    reescrevendo o identity

--- Services/Identity/Api/SeedWork/PaginationQueryParams.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.SeedWork
-{
-    public class PaginationQueryParams
-    {
-        public int PageIndex { get; set; } = 0;
-        public int PageSize { get; set; } = 10;
-        public string Sort {get; set;}
-    }
-}
--- Services/Identity/Domain/SeedWork/IAggregateRoot.cs
+++
@@ -1,4 +0,0 @@
-namespace Domain.SeedWork
-{
-    public interface IAggregateRoot { }
-}
